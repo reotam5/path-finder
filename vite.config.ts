90d8e170dff46cddf31ac613ef8c565ff84b859c
Reo Tamai <41591313+reotam5@users.noreply.github.com>
2025-09-24 22:44:48 -0700

    fix base url for deployed case

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/path-finder/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
